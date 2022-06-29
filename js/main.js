5eaf3732c00a20bc83f6a1ef0920896451ac9b75
ihorborys <ihorborys@gmail.com>
2022-06-29 13:33:54 +0300

    Add hold functionality

--- js/main.js
+++ js/main.js
@@ -4,15 +4,21 @@ const dice = document.querySelector(".dice");
 const diceImg = dice.children[0];
 const rollDiceButton = document.querySelector(".roll-dice");
 const holdButton = document.querySelector(".hold");
-let activePlayer;
 let currentScore = 0;
+let activePlayer;
+
+newGameButton.addEventListener("click", onStartNewGameBtnClick);
+rollDiceButton.addEventListener("click", onRollDiceBtnClick);
+holdButton.addEventListener("click", onHoldBtnClick);
 
 function getRandomNumber(topValue) {
     return Math.floor(Math.random() * topValue) + 1;
 }
 
 function onStartNewGameBtnClick() {
+    resetTotalScore();
     players[0].classList.add("active");
+    players[1].classList.remove("active");
     activePlayer = players[0];
     dice.style.display = "block";
     rollDiceButton.style.display = "flex";
@@ -20,26 +26,49 @@ function onStartNewGameBtnClick() {
 }
 
 function onRollDiceBtnClick() {
-    const imgNumber = getRandomNumber(6);
-
+    const randomNumber = getRandomNumber(6);
+    
     // const srcValue = "./assets/images/dice-" + imgNumber + ".png";
-    const srcValue = `./assets/images/dice-${imgNumber}.png`;
+    const srcValue = `./assets/images/dice-${randomNumber}.png`;
     diceImg.setAttribute("src", srcValue);
     
-    if (imgNumber === 1) {
-        currentScore = 0;
-        updateCurrentScore();
-        toggleActivePlayer();
+    if (randomNumber === 1) {
+        resetCurrentScore();
     } else {
-        // currentScore = currentScore + imgNumber;
-        currentScore += imgNumber;
-        updateCurrentScore();
+        updateCurrentScore(randomNumber);
     }
 }
 
-function updateCurrentScore() {
+function onHoldBtnClick() {
+    updateTotalScore();
+    resetCurrentScore();
+}
+
+function updateCurrentScore(randomNumber) {
     const currentScoreValue = activePlayer.querySelector(".score");
+    // currentScore = currentScore + randomNumber;
+    currentScore += randomNumber;
     currentScoreValue.innerHTML = currentScore;
+}
+
+function resetCurrentScore() {
+    currentScore = 0;
+    updateCurrentScore(0);
+    toggleActivePlayer();
+}
+
+function updateTotalScore() {
+    const totalScoreValue = activePlayer.querySelector(".totalscore");
+    let totalScore = Number(totalScoreValue.innerHTML);
+    totalScore += currentScore;
+    totalScoreValue.innerHTML = totalScore;
+}
+
+function resetTotalScore() {
+    players.forEach((player) => {
+        const totalScoreValue = player.querySelector(".totalscore");
+        totalScoreValue.innerHTML = 0;
+    });
 }
 
 function toggleActivePlayer() {
@@ -53,5 +82,3 @@ function toggleActivePlayer() {
     });
 }
 
-newGameButton.addEventListener("click", onStartNewGameBtnClick);
-rollDiceButton.addEventListener("click", onRollDiceBtnClick);
